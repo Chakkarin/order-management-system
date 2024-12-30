15b8a7dcb42d1b50abf09b9d83095fa1662e537b
Chakkarin <chakkarin.yin@gmail.com>
2024-12-30 19:46:05 +0700

    comment mark send message kafka

--- services/auth-service/internal/usecases/user_usecase.go
+++ services/auth-service/internal/usecases/user_usecase.go
@@ -25,24 +25,24 @@ func NewUserUsecase(repo domain.UserRepository, redis *redis.Client) UserUsecase
 
 func (u *UserUsecase) Register(ctx context.Context, user *domain.User) error {
 
-	// 1. ตรวจสอบ Username และ Email ว่าซ้ำหรือไม่
+	// ตรวจสอบ Username และ Email ว่าซ้ำหรือไม่
 	if err := u.UserRepo.CheckDuplicate(ctx, user.Username, user.Email); err != nil {
 		return errors.New("username or email already exists")
 	}
 
-	// 2. Hash Password
+	// Hash Password
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
 		return errors.New("failed to hash password")
 	}
 	user.Password = string(hashedPassword)
 
-	// 3. บันทึก User ลงฐานข้อมูล
+	// บันทึก User ลงฐานข้อมูล
 	if err := u.UserRepo.CreateUser(ctx, user); err != nil {
 		return errors.New("failed to create user")
 	}
 
-	// 4. สร้าง Verification Code และเก็บใน Redis
+	// สร้าง Verification Code และเก็บใน Redis
 	verificationCode, err := utils.GenerateVerificationCode()
 	if err != nil {
 		return errors.New("failed to generate verification code")
@@ -52,6 +52,8 @@ func (u *UserUsecase) Register(ctx context.Context, user *domain.User) error {
 	if err != nil {
 		return errors.New("failed to store verification code")
 	}
+
+	// ส่ง kafka
 
 	return nil
 }
